08527e1ce2181bc51a1100b2692cd65b135bb7ed
Nikhil Mane <113436530+nikhilmane81@users.noreply.github.com>
2023-01-11 23:18:07 +0530

    Add files via upload

---
+++ MS.NET Assignments/Assignment3/Assignment3/CodeRunner.cs
@@ -0,0 +1,17 @@
+﻿namespace Assignment3
+{
+    internal class CodeRunner
+    {
+        static void Main(string[] args)
+        {
+            Manager mng = new Manager("Mayur", 23, "Manager");
+            mng.Display();
+            GeneralManager gm = new GeneralManager("Manish", 20, "Genral-Manager", "Free Food forever");
+            gm.Display();
+            Ceo ceo = new Ceo("Nikhil", 1);
+            ceo.Display();
+        }
+    }
+
+
+}
---
+++ MS.NET Assignments/Assignment3/Assignment3/Manager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    public class Manager : Employee,IDbFuntions
+    {
+        public override decimal Basic 
+        {
+            get
+            {
+                return 4500;
+            }
+        }
+
+
+
+        public Manager(string Name, short DeptNo, string Designation) : base(Name, DeptNo)
+        {
+            this.Designation= Designation;   
+        }
+
+        private string designation="";
+        public string Designation
+        {
+            set
+            {
+                if(value.Length> 0)
+                {
+                    designation= value;
+                }
+                else
+                {
+                    Console.WriteLine("designation can not be blank");
+                }
+            }
+            get
+            { return designation; } 
+        }
+
+        public override decimal CalcNetSalary()
+        {
+            return Basic * 2 + 4390;
+        }
+
+        public override void Insert()
+        {
+            Console.WriteLine("Manager Insert()");
+        }
+
+        public override void Update()
+        {
+            Console.WriteLine("Manager Update()");
+        }
+
+        public override void Delete()
+        {
+            Console.WriteLine("Manager Delete()");
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("[ EmpNo= " + this.EmpNo + " Name= " + this.Name + "  DeptNo= " + this.DeptNo + " Basic= " + this.Basic+" Designation= " + this.Designation + " NetSalary= " + this.CalcNetSalary());
+        }
+    }
+}
